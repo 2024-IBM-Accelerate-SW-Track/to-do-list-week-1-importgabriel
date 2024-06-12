e0f3b0630ccd5ee3dfda14ae3110333888527ec4
importgabriel <mgg42999@uga.edu>
2024-06-12 16:35:26 -0400

    initial commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-  
+
+import "./About.css";
+
+import profilePic from "../assets/profile_pic_name.jpg";
+
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+      <div className="split left">
+        <div className="centered">
+          <img
+            className="profile_image"
+            src={profilePic}
+            alt="Profile Pic"
+          />
+        </div>
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Miguel Gabriel Gutierrez</div>
+          <div className="brief_description">
+            Aspiring Software Engineer
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
